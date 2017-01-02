5fe86a1558a9e84f79602a186adb249e6587819f
George Smith-Sweeper <george.smsweeper@gmail.com>
2017-01-02 16:09:48 -0500

    astroids explode, and players ship explodes

--- Spaceshooter/Assets/Scripts/DestoryByContact.cs
+++ Spaceshooter/Assets/Scripts/DestoryByContact.cs
@@ -3,11 +3,18 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class DestoryByContact : MonoBehaviour {
+	public GameObject explosion;
+	public GameObject playerExplosion;
 
 	void OnTriggerEnter(Collider other) {
 		// returns control back to Unity's game loop
 		if (other.tag == "Boundary") {
 			return;
+		}
+
+		Instantiate (explosion, transform.position, transform.rotation);
+		if (other.tag == "Player") {
+			Instantiate (playerExplosion, other.transform.position, other.transform.rotation);
 		}
 		//destroys the bolt, and the asteroid once they come in contact with each other
 		Destroy(other.gameObject);
